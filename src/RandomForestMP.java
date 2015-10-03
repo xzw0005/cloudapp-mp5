58832af8678fa23cbbd950d22c0cb35e3327ba73
Xing Wang <xzw0005@tigermail.auburn.edu>
2015-10-03 18:15:08 -0500

    cao

--- src/RandomForestMP.java
+++ src/RandomForestMP.java
@@ -10,6 +10,7 @@ import org.apache.spark.mllib.regression.LabeledPoint;
 import org.apache.spark.mllib.tree.model.RandomForestModel;
 import org.apache.spark.mllib.tree.RandomForest;
 
+import scala.Tuple2;
 
 import java.util.HashMap;
 import java.util.regex.Pattern;
@@ -35,7 +36,7 @@ public final class RandomForestMP {
 	private static class DataToFeatureVector implements Function<String, Vector> {
 		private static final Pattern SPACE = Pattern.compile(",");
 		
-		public Vector call(String line) {
+		public Vector call(String line) throws Exception {
 			String[] token = SPACE.split(line);
 			double[] point = new double[token.length - 1];
 			for (int i = 0; i < token.length - 1; ++i) {
